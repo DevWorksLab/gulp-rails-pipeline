0b13227edf0fd036928de4528fd1252249873c2e
Alex Hasapis <alex@nycdevshop.com>
2016-04-25 20:38:36 -0400

    remove unused

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.5.0'
 
 set :application, 'motivatedesign'
-set :repo_url, 'git@github.com:DevWorksLab/motivatedesign.git'
+set :repo_url, 'git@github.com:DevWorksLab/gulp-rails-pipeline.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
