e9ced3b5dbfc43edd823a1267020195cff873215
Alex Hasapis <alex@nycdevshop.com>
2016-05-17 14:32:19 -0400

    validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,8 +1,8 @@
 class Person < ActiveRecord::Base
   validates :first_name, :last_name, :bio, :email, :linkedin, :twitter, :title,
-  :snippet, :header_pic, :profile_pic, :skill_one, :skill_one_icon,
-  :skill_one_blurb, :skill_two, :skill_two_icon, :skill_two_blurb, :skill_three,
-  :skill_three_icon, :skill_three_blurb, presence: true
+  :snippet, :skill_one,
+  :skill_one_blurb, :skill_two, :skill_two_blurb, :skill_three,
+  :skill_three_blurb, presence: true
 
   scope :active, -> { where(active: true) }
   scope :order_by_active, -> { order(:active => "desc" , :priority => "asc") }
