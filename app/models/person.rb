04c76730efa7244887280848b81193e4da4141d5
Alex Hasapis <alex@nycdevshop.com>
2016-05-17 18:35:15 -0400

    import people

--- app/models/person.rb
+++ app/models/person.rb
@@ -23,4 +23,46 @@ class Person < ActiveRecord::Base
     "#{self.first_name} " + "#{self.last_name}"
   end
 
+  def self.import_csv
+    file = File.open Rails.root.join('motivatepeople.csv')
+    CSV.foreach file.path do |row|
+      unless row[0] == "id"
+        first_name = row[1]
+        last_name = row[2]
+        bio = row[3]
+        email = row[4]
+        linkedin row[5]
+        twitter = row[6]
+        title = row[7]
+        snippet = row[8]
+        skill_one = row[9]
+        skill_one_blurb = row[10]
+        skill_two = row[11]
+        skill_two_blurb = row[12]
+        skill_three = row[13]
+        skill_three_blurb = row[14]
+        priority = row[17]
+        active = row[18]
+        Person.create(
+          first_name: first_name,
+          last_name: last_name,
+          bio: bio,
+          email: email,
+          linkedin: linkedin,
+          twitter: twitter,
+          title: title,
+          snippet: snippet,
+          skill_one: skill_one,
+          skill_one_blurb: skill_one_blurb,
+          skill_two: skill_two,
+          skill_two_blurb: skill_two_blurb,
+          skill_three: skill_three,
+          skill_three_blurb: skill_three_blurb,
+          priority: priority,
+          active: active
+        )
+      end
+    end
+  end
+
 end
