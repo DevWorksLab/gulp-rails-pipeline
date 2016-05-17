7b5f719019941b44c792d00013326d6c65cbfe73
Alex Hasapis <alex@nycdevshop.com>
2016-05-17 11:25:27 -0400

    related links

--- app/controllers/uxhires/jobs_controller.rb
+++ app/controllers/uxhires/jobs_controller.rb
@@ -1,6 +1,7 @@
 class Uxhires::JobsController < ApplicationController
-  before_action :active_jobs, only: [:uxhires, :index, :refine, :job_show]
+  before_action :active_jobs, only: [:uxhires, :index, :refine, :job_show, :job_app]
   before_action :set_nested_job, only: [:job_app, :apply]
+  before_action :set_external_links, only: [:job_show, :job_app, :index]
 
   def uxhires
     @open = @active.sample(4)
@@ -10,19 +11,17 @@ class Uxhires::JobsController < ApplicationController
 
   def index
     @jobs = @active.paginate(:page => params[:page], :per_page => 5)
-
-
   end
 
   def job_show
     @job = Job.find(params[:id])
-    @open = @active.where("location LIKE ? OR title LIKE ?", @job.location, @job.title).sample(4)
-    @featured = @open.shift
+    set_related
   end
 
   def job_app
     cats = CatsOne.new( options: {id: @job.catsone_id, form: true} )
     @form = cats.get_application
+    set_related
     # if @form["response"]["item"] && @form["response"]["item"]["name"] != "General Application"
     #   binding.pry
     # end
@@ -54,6 +53,16 @@ class Uxhires::JobsController < ApplicationController
 
     def set_nested_job
       @job = Job.find(params[:job_id])
+    end
+
+    def set_related
+      @open = @active.where("location LIKE ? OR title LIKE ?", @job.location, @job.title).sample(4)
+      @featured = @open.shift
+    end
+
+    def set_external_links
+      @external_links = ExternalLink.take(3)
+      @featured_person = Person.active.sample
     end
 
 end
